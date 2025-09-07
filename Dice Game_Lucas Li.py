7d317acd12f82d46ed526531f59050bbce529554
Gitisgoodd <lilucas435@gmail.com>
2025-09-07 10:56:08 -0700

    Update Dice Game_Lucas Li.py

--- Dice Game_Lucas Li.py
+++ Dice Game_Lucas Li.py
@@ -1,3 +1,13 @@
+#Project Reflection
+# “What was the hardest bug to fix, and why?”
+# The hardest bug to fix was the grade checker, where there were lots of places where it could have been a lot more simplifier. So I had to look at the whole code and use logic to see what places weren't needed and which were needed.
+
+# “How did testing help you find logical problems?”
+# Testing helped me find logical problems since after testing the code for a couple of times, I would see which places would be correct, and which places don't make that much logical sense.
+
+# “What is one thing you’ll do in future projects to avoid writing sloppy code?”
+# One thing that I will do in future projects to aviod writing sloppy code is to dedicate one day or a good amount of time just to review and go over the code to see which parts could be ommitted out since it would be unnecessary.
+
 #Title of the program
 print("Dice Game Result Checker")
 
@@ -16,6 +26,6 @@ while True :
             roll=+1
         elif roll==2 or roll==1:
             print("You win something small")
-#If the inpute is not in the range of 1-6 or not a number
+#If the input is not in the range of 1-6 or not a number
     else:
         print ("An invalid input, please put a number between 1-6 ")
